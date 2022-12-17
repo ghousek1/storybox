d4bce0b26fdeb2be513f839522f458fdce3b6cf7
ghousek1 <ghousek1@outlook.com>
2022-12-17 16:32:06 +0530

    mongo db url added

--- src/main/java/com/ghouse/storybox/controller/HealthCheckController.java
+++ src/main/java/com/ghouse/storybox/controller/HealthCheckController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ghouse.storybox.config.ConfigUtil;
@@ -20,13 +21,11 @@ public class HealthCheckController {
         return new ResponseEntity<>("SUCCESS", HttpStatus.OK);
     }
     
-    @GetMapping("/envCheck")
-    public ResponseEntity<String> envCheck() {
+    @GetMapping("/env-check")
+    public ResponseEntity<String> envCheck(@RequestParam String envKey) {
     
-    	String envValue =null;
-    	envValue = configUtil.getProperty("custom.ghouse.url");
+    	String envValue = configUtil.getProperty(envKey);
     			
-    	
         return new ResponseEntity<>(envValue, HttpStatus.OK);
     }
 
