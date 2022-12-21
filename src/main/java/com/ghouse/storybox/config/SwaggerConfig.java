a4bf3111d93dcf4700d9ebe75dc18d421d1be477
ghousek1 <ghousek1@outlook.com>
2022-12-21 08:31:52 +0530

    swagger config corrected

--- src/main/java/com/ghouse/storybox/config/SwaggerConfig.java
+++ src/main/java/com/ghouse/storybox/config/SwaggerConfig.java
@@ -4,6 +4,7 @@ import java.util.Collections;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
@@ -13,6 +14,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
+@EnableWebMvc
 public class SwaggerConfig {
 
 	@Bean
